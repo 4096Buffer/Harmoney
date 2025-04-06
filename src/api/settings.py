ca181f0f23913eb78beefef4ac7aae9fb7849bc6
Buffer4096 <natan.natan.2011.nn@gmail.com>
2025-04-06 15:33:07 +0200

    new gitignore

--- src/api/settings.py
+++ src/api/settings.py
@@ -7,7 +7,7 @@ db_host = "localhost"
 db_port = "5432"
 db_name = "postgres"
 
-SECRET_KEY = "86467b0635b3824bc703b2eb76da4cc23d3e4ad31ee74c8b83a0cf5b88f0f7a84265e1bcb4f62cc567685bf9419adc1d8d3c2a67d160d8d0f7d15493a5456be3"
+SECRET_KEY = "vws80kzedXgWvtxVngsEewhcK1RRTy8VRxssHSW1yPFj6YxrFkZTEdMYeM9_Q48v"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRES = 15  # minutes
 REFRESH_TOKEN_EXPIRES = 7  # days
